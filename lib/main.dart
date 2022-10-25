b3267e637eaf48e9360f866106722aad01b8e8c3
ryo0213 <ryodayo0213@gmail.com>
2022-10-25 22:56:23 +0900

    アスペクト比率でアプリ画面縮小

--- lib/main.dart
+++ lib/main.dart
@@ -44,17 +44,27 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          height: 300,
+          width: 200,
+          color: Colors.amber,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: 1/2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
